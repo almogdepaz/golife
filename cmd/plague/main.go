0b2bd08bb56a1ab44b03a41a4ecc8c0157e23d1e
almogs <almog@ras-robotics.com>
2018-08-11 18:32:33 +0300

    added palgue mode + cmd args for running

--- cmd/plague/main.go
+++ cmd/plague/main.go
@@ -10,12 +10,15 @@ import (
 )
 
 func NextV2(f *golife.Field, x, y int) bool {
+	//if cell is alive and ha no living horizontal neighbors return false
 	if f.Alive(x, y) && !f.Alive(x+1, y) && !f.Alive(x-1, y) && !f.Alive(x, y+1) && !f.Alive(x, y-1) {
 		return false
 	}
-	if f.CountLiveNeighbors(x, y) == 1 {
+	//if cell is dead but has exactly one living neighbor return true
+	if !f.Alive(x, y) && f.CountLiveNeighbors(x, y) == 1 {
 		return true
 	}
+	//state remains the same
 	return f.Alive(x, y)
 }
 
@@ -28,14 +31,14 @@ func main() {
 	maxGen, _ := strconv.Atoi(argsWithoutProg[3])
 	seed := strings.Split(argsWithoutProg[4], " ")
 	if len(seed) != w*h {
-		fmt.Println("bad arguments")
+		fmt.Println("bad seed langth ")
 		return
 	}
 	seedArray := make([]bool, 0, h*w)
 	for _, e := range seed {
 		num, err := strconv.Atoi(e)
-		if err != nil {
-			fmt.Println("bad arguments")
+		if err != nil || (num != 0 && num != 1) {
+			fmt.Println("bad seed content")
 			return
 		}
 		seedArray = append(seedArray, num == 1)
