e0955e75e6d17c21bef90a21080e363b9f1b9eb1
almogs <almog@ras-robotics.com>
2018-08-12 12:28:15 +0300

    add step test

--- life_test.go
+++ life_test.go
@@ -6,14 +6,25 @@ import (
 )
 
 var (
-	input    = []bool{false, false, false, true, true, true, false, true, false}
-	expected = "0 0 0 1 1 1 0 1 0"
+	input           = []bool{false, false, false, true, true, true, false, true, false}
+	expectedStr     = "0 0 0 1 1 1 0 1 0"
+	stepInput       = []bool{false, false, false, true, false, false, true, false, true,}
+	expectedStepStr = "0 0 0 0 1 0 0 1 0"
 )
 
 func TestString(t *testing.T) {
 	l := NewLife(3, 3, input, Next)
 	str := l.String()
-	if strings.Compare(str, expected) != 0 {
-		t.Error("String formatting error result was ", str, " expected ", expected)
+	if strings.Compare(str, expectedStr) != 0 {
+		t.Error("String formatting error result was ", str, " expected ", expectedStr)
+	}
+}
+
+func TestStep(t *testing.T) {
+	l := NewLife(3, 3, stepInput, Next)
+	l.Step()
+	str := l.String()
+	if strings.Compare(l.String(), expectedStepStr) != 0 {
+		t.Error("String formatting error result was ", str, " expected ", expectedStepStr)
 	}
 }
